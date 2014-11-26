7995033b94d7a2eadfc512aaffe1462f086edef5
urise <urise@i.ua>
2014-11-26 14:31:12 +0200

    IniFileSupport has been added

---
+++ Pomodoro/BusinessLogic/IniConfig.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class IniConfig
+    {
+        #region Constructors
+
+        public IniConfig()
+        {
+
+        }
+
+        public IniConfig(string fileName)
+        {
+            var lines = File.ReadAllLines(fileName);
+            InitFromLines(lines);
+        }
+
+        public IniConfig(IEnumerable<string> lines)
+        {
+            InitFromLines(lines);
+        }
+
+        private void InitFromLines(IEnumerable<string> lines)
+        {
+            foreach (var line in lines)
+                ProcessIniLine(line);
+        }
+
+        #endregion
+
+        #region Public Members
+
+        public string this[string name]
+        {
+            get
+            {
+                string value;
+                return _dict.TryGetValue(name, out value) ? value : string.Empty;
+            }
+            set
+            {
+                if (ForbidNewKeys && !_dict.ContainsKey(name))
+                    throw new Exception("New keys are forbidden");
+                _dict[name] = value;
+            }
+        }
+
+        public int Count
+        {
+            get { return _dict.Count; }
+        }
+
+        public bool ForbidNewKeys { get; set; }
+
+        public List<string> GetIniLines()
+        {
+            return _dict.Select(r => r.Key + '=' + r.Value).ToList();
+        }
+
+        #endregion
+
+        #region Private Members
+
+        private Dictionary<string, string> _dict = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+        private void ProcessIniLine(string line)
+        {
+            var index = line.IndexOf('=');
+            if (index < 0) return;
+            var key = line.Substring(0, index).Trim();
+            var value = line.Substring(index + 1).Trim();
+            _dict[key] = value;
+        }
+
+        #endregion
+    }
+}
---
+++ Pomodoro/UnitTests/BusinessLogic/IniFileTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic;
+using NUnit.Framework;
+
+namespace UnitTests.BusinessLogic
+{
+    [TestFixture]
+    public class IniFileTests
+    {
+        private List<string> _testLines = new List<string>
+            {
+                "ABC=123",
+                "defgh = true"
+            };
+        [Test]
+        public void ConstructorTest()
+        {
+            var iniConfig = new IniConfig(_testLines);
+            Assert.That(iniConfig.Count, Is.EqualTo(2));
+            Assert.That(iniConfig["ABC"], Is.EqualTo("123"));
+            Assert.That(iniConfig["abc"], Is.EqualTo("123"));
+            Assert.That(iniConfig["Defgh"], Is.EqualTo("true"));
+            Assert.That(iniConfig["wrongKey"], Is.Empty);
+        }
+
+        [Test]
+        public void SetExistingTest()
+        {
+            var iniConfig = new IniConfig(_testLines);
+            iniConfig["abc"] = "456";
+            Assert.That(iniConfig["ABC"], Is.EqualTo("456"));
+        }
+
+        [Test]
+        public void SetNewKeyTest()
+        {
+            var iniConfig = new IniConfig(_testLines);
+            iniConfig["newKey"] = "newValue";
+            Assert.That(iniConfig["NEWKEY"], Is.EqualTo("newValue"));
+        }
+
+        [Test]
+        [ExpectedException]
+        public void SetForbiddenNewKeyTest()
+        {
+            var iniConfig = new IniConfig(_testLines);
+            iniConfig.ForbidNewKeys = true;
+            iniConfig["newKey"] = "newValue";
+        }
+
+        [Test]
+        public void GetIniLinesTest()
+        {
+            var iniConfig = new IniConfig(_testLines);
+            iniConfig["newKey"] = "newValue";
+            iniConfig["abc"] = "456";
+            CollectionAssert.AreEquivalent(iniConfig.GetIniLines(),
+                new List<string>
+                {
+                    "ABC=456",
+                    "defgh=true",
+                    "newKey=newValue"
+                });
+        }
+    }
+}
